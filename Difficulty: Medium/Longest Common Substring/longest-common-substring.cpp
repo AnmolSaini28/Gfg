adcb813070e67bd521a882a7d2851dcbc972ae95
Anmol Saini <143955945+AnmolSaini28@users.noreply.github.com>
2025-02-03 13:40:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+  
+    int longestCommonSubstr(string& s1, string& s2) {
+        int n = s1.size();
+        int m = s2.size();
+        
+        vector <vector <int>> dp(n+1, vector <int> (m+1, 0));
+        
+        int ans = 0;
+
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=m ; j++){
+                if(s1[i-1] == s2[j-1]){
+                    int val = dp[i-1][j-1]+1;
+                    dp[i][j] = val;
+                    ans = max(ans,val);
+                }
+                else{
+                    dp[i][j] = 0;
+                }
+            }
+        
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s1, s2;
+        cin >> s1 >> s2;
+        Solution ob;
+
+        cout << ob.longestCommonSubstr(s1, s2) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+}
+
+// } Driver Code Ends
