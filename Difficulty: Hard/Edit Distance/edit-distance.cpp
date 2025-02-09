5f4ecf281a193c7700a34cf884b079a1726768df
Anmol Saini <143955945+AnmolSaini28@users.noreply.github.com>
2025-02-09 11:17:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Edit Distance/edit-distance.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to compute the edit distance between two strings
+    int editDistanceUtil(string& S1, string& S2, int i, int j, vector<vector<int>>& dp) {
+    if (i < 0)
+        return j + 1;
+    if (j < 0)
+        return i + 1;
+
+    if (dp[i][j] != -1)
+        return dp[i][j];
+
+    if (S1[i] == S2[j])
+        return dp[i][j] = 0 + editDistanceUtil(S1, S2, i - 1, j - 1, dp);
+
+    
+    else
+        return dp[i][j] = 1 + min(editDistanceUtil(S1, S2, i - 1, j - 1, dp),
+                                  min(editDistanceUtil(S1, S2, i - 1, j, dp),
+                                      editDistanceUtil(S1, S2, i, j - 1, dp)));
+}
+    int editDistance(string& S1, string& S2) {
+        
+        int n = S1.size();
+    int m = S2.size();
+
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+
+    return editDistanceUtil(S1, S2, n - 1, m - 1, dp);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;
+    cin.ignore();
+    while (T--) {
+        string s1;
+        getline(cin, s1);
+        string s2;
+        getline(cin, s2);
+        Solution ob;
+        int ans = ob.editDistance(s1, s2);
+        cout << ans << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
