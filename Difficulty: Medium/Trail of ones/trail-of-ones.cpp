38187c9eeeda70e2e435a080406cb8525f5647f4
Anmol Saini <143955945+AnmolSaini28@users.noreply.github.com>
2025-07-11 23:58:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Trail of ones/trail-of-ones.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int solve(int n , int index){
+        if(index>n){
+            return 1;
+        }
+        int res= solve(n,index+1) + solve(n,index+2);
+        return res;
+    }
+    int countConsec(int n) {
+        // code here
+        int total= 1<<n;
+        int noconsecutive = solve(n,1);
+        
+        int ans=total - noconsecutive;
+        
+        return ans;
+    }
+};
