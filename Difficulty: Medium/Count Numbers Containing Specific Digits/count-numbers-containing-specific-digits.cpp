42522f1b164d013329d3dd0572b331218b65009e
Anmol Saini <143955945+AnmolSaini28@users.noreply.github.com>
2025-07-20 23:54:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Numbers Containing Specific Digits/count-numbers-containing-specific-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int countValid(int n, vector<int>& arr) {
+        // code here
+        bool zero = 0;
+    for(auto it: arr){
+        if(it==0)zero =1;
+    }
+     int total = 9*pow(10,n-1);
+     int nott = pow(10-arr.size(),n);
+     
+     if(zero==0)
+     {
+         nott-= pow(10-arr.size(),n-1);
+     }
+     return total-nott;
+    }
+};
